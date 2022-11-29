37e54046938b3b92d8f3e374638317f57ef405cd
Andriy Makarenko <andriy@mate.academy>
2022-11-29 20:50:56 +0200

    Third attempt

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,12 +1,27 @@
 import datetime
 
 from django.db import models
+from user.models import User
 
 
 class Borrowing(models.Model):
-    borrow_date = models.DateField(auto_created=True)
-    # expected_return_date = borrow_date
-                           # + datetime.timedelta(days=30)
-    actual_return_date = models.DateField(auto_now_add=True)
-    # book_id = models.OneToOneField(Book, on_delete=models.CASCADE, related_name="books")
-    # user_id = models.ForeignKey(AuthUserModel, on_delete=models.CASCADE, related_name="users")
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(blank=True, null=True)
+    # book = models.OneToOneField(Book, on_delete=models.CASCADE, related_name="books")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users")
+
+    def clean(self):
+        self.expected_return_date = self.borrow_date + datetime.timedelta(days=10)
+
+    def save(
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None,
+    ):
+        self.full_clean()
+        return super(Borrowing, self).save(
+            force_insert, force_update, using, update_fields
+        )
