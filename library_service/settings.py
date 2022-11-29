1988f94817727a3a6f9cda084575dd13aa972ef9
Liudmyla Kulinchenko <liudmilakulinchenko@gmail.com>
2022-11-29 17:35:59 +0100

    Implement CRUD functionality for Books Service

--- library_service/settings.py
+++ library_service/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "library",
 ]
 
 MIDDLEWARE = [
@@ -54,8 +56,7 @@ ROOT_URLCONF = "library_service.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
-        ,
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
