c03abe6c7a2467515207e5f3f18d1a60d3f6371a
Kirill Drago <dragoswim7@gmail.com>
2022-11-30 22:16:32 +0200

    Fix bug with password

--- user/serializers.py
+++ user/serializers.py
@@ -4,11 +4,17 @@ from django.utils.translation import gettext as _
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        style={"input_type": "password"},
+        write_only=True,
+        min_length=5
+    )
+
     class Meta:
         model = get_user_model()
         fields = ("id", "email", "password", "is_staff")
         read_only_fields = ("is_staff",)
-        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+        # extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
