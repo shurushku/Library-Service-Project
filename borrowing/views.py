175c526fa0d2527b6f9674575c9b1a6905686871
Andriy Makarenko <andriy@mate.academy>
2022-12-01 18:19:26 +0200

    Add is_acitve $ user_id filtering

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,4 +1,4 @@
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, viewsets, filters
 from rest_framework.permissions import IsAuthenticated
 
 from borrowing.models import Borrowing
@@ -13,14 +13,27 @@ class BorrowingViewSet(
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
     mixins.CreateModelMixin,
-    viewsets.GenericViewSet
+    viewsets.GenericViewSet,
 ):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingListSerializer
     permission_classes = (IsAuthenticated,)
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["book__title", "id"]
 
     def get_queryset(self):
         queryset = self.queryset.select_related("book")
+        is_active = self.request.query_params.get("is_active")
+        user_id = self.request.query_params.get("user_id")
+
+        if is_active:
+            if is_active == "true":
+                queryset = queryset.filter(actual_return_date__isnull=True)
+            if is_active == "false":
+                queryset = queryset.filter(actual_return_date__isnull=False)
+
+        if user_id:
+            queryset = queryset.filter(user_id=user_id)
 
         if self.action == "list" and not self.request.user.is_staff:
             return queryset.filter(user=self.request.user)
